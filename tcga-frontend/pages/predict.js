abfbb575ea6ee0d75ba748699e98a6e256975c60
Bill Nguyen <BILLNT0711@GMAIL.COM>
2023-11-24 19:48:16 -0800

    Working on onSubmit

--- tcga-frontend/pages/predict.js
+++ tcga-frontend/pages/predict.js
@@ -2,97 +2,127 @@ import Head from "next/head";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import SectionTitle from "../components/sectionTitle";
+import { useFormik } from 'formik';
+import axios, { AxiosError } from 'axios';
+import React, { useState } from "react";
 
 const Predict = () => {
-    return (
-      <>
-        <Head>
-          <title>tcg.ai - Cancer Classification Using Genomic Data</title>
-          <meta
-            name="description"
-            content="tcg.ai is an AI tool to predict cancer types based on genomic data"
-          />
-          <link rel="icon" href="/tcgai_icon.png" />
-        </Head>
-        <Navbar />
-        <SectionTitle
-          pretitle="TCG.AI"
-          title="Predict Cancer Type">
-          Input patient information and any available simple somatic mutations.
-        </SectionTitle>
-        <div className="flex w-auto flex-col px-48">
-          <InputForm />
-        </div>
-        <Footer />
-      </>
-    );
-  }
+  const [error, setError] = useState('');
+  const somaticMutations = ['TP53', 'PIK3CA', 'TTN', 'CDH1', 'GATA3', 'APC', 'KRAS', 
+                            'SYNE1', 'CSMD3', 'MUC16', 'RYR2', 'PTEN', 'NRAS', 'MUC5B', 'TET2', 
+                            'PTPN11', 'NOTCH1', 'FBXW7', 'PHF6', 'IGHV270', 'IGLV31', 'IGHV270D', 
+                            'DNMT3A', 'NPM1', 'FLT3', 'IDH2', 'RUNX1', 'EGFR', 'IDH1', 'ATRX', 'CIC', 
+                            'ARID1A', 'VHL', 'PBRM1', 'SETD2', 'BAP1', 'MET', 'KMT2C', 'PKHD1', 'ICE1', 
+                            'SMAD4', 'CDKN2A', 'BRAF', 'DNAH5', 'PCLO', 'LRP2', 'FREM2', 'FLG', 'TG', 'HRAS', 
+                            'KMT2D', 'BTG2', 'B2M', 'PIM1', 'IGHG1'];
+
+  const initialSSMSVals = JSON.parse('{}');
   
-  function InputForm() {
-    return (
-      <form class="flex flex-col bg-gray-25 shadow-md rounded px-60 pt-6 pb-8 mb-4 dark:bg-gray-700 dark:border-gray-600 dark:text-white items center justify center">
-        <h3 class="mb-4 font-semibold text-teal-500 dark:text-white text-start">Patient Race/Ethnicity</h3>
-        <select id="races" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500">
-          <option>White</option>
-          <option>Black</option>
-          <option>Asian</option>
-          <option>Other</option>
-        </select>
+  somaticMutations.forEach((ssms) => initialSSMSVals.push(ssms + ": '0'"))
 
-        <h3 class="pt-8 mb-4 font-semibold text-teal-500 dark:text-white text-start">Patient Age</h3>
-        <input type="number" id="age" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500"></input>
-
-        <h3 class="pt-8 mb-4 font-semibold text-teal-500 dark:text-white text-start">Prior Malignancy</h3>
-        <div class="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
-            <input id="PMYes" type="radio" value="" name="PriorMalignancy" class="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-            <label for="PMYes" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Yes</label>
-        </div>
-        <div class="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
-            <input id="PMNo" type="radio" value="" name="PriorMalignancy" class="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-            <label for="PMNo" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">No</label>
-        </div>
-
-        <h3 class="pt-8 mb-4 font-semibold text-teal-500 dark:text-white text-start">Synchronous Malignancy</h3>
-        <div class="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
-            <input id="SMYes" type="radio" value="" name="SynchronousMalignancy" class="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-            <label for="SMYes" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Yes</label>
-        </div>
-        <div class="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
-            <input id="SMNo" type="radio" value="" name="SynchronousMalignancy" class="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-            <label for="SMNo" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">No</label>
-        </div>
-
-        <h3 class="pt-8 mb-4 font-semibold text-teal-500 dark:text-white text-start">Simple Somatic Mutations</h3>
-        <SSMSCheckboxes />
-
-        <button class="flex w-36 mt-12 mb-8 bg-teal-500 hover:bg-teal-400 text-white font-bold py-2 px-4 rounded items-center justify-center content-center" type="submit">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
-          </svg>
-          <span class="pl-2 text-lg">Predict</span>
-        </button>
-
-      </form>
-    );
+  const initialOtherVals = {
+    races: 'White',
+    age:'',
+    PriorMalignancy:'',
+    SynchronousMalignancy:'',
+    // TP53: '0', PIK3CA: '0', TTN: '0', CDH1: '0', GATA3: '0', APC: '0', KRAS: '0', 
+    // SYNE1: '0', CSMD3: '0', MUC16: '0', RYR2: '0', PTEN: '0', NRAS: '0', MUC5B: '0', TET2: '0', 
+    // PTPN11: '0', NOTCH1: '0', FBXW7: '0', PHF6: '0', IGHV270: '0', IGLV31: '0', IGHV270D: '0', 
+    // DNMT3A: '0', NPM1: '0', FLT3: '0', IDH2: '0', RUNX1: '0', EGFR: '0', IDH1: '0', ATRX: '0', CIC: '0', 
+    // ARID1A: '0', VHL: '0', PBRM1: '0', SETD2: '0', BAP1: '0', MET: '0', KMT2C: '0', PKHD1: '0', ICE1: '0', 
+    // SMAD4: '0', CDKN2A: '0', BRAF: '0', DNAH5: '0', PCLO: '0', LRP2: '0', FREM2: '0', FLG: '0', TG: '0', HRAS: '0', 
+    // KMT2D: '0', BTG2: '0', B2M: '0', PIM1: '0', IGHG1: '0',
   }
 
-  function SSMSCheckboxes() {
-    var somaticMutations = ['TP53', 'PIK3CA', 'TTN', 'CDH1', 'GATA3', 'APC', 'KRAS', 
-                    'SYNE1', 'CSMD3', 'MUC16', 'RYR2', 'PTEN', 'NRAS', 'MUC5B', 'TET2', 
-                    'PTPN11', 'NOTCH1', 'FBXW7', 'PHF6', 'IGHV2-70', 'IGLV3-1', 'IGHV2-70D', 
-                    'DNMT3A', 'NPM1', 'FLT3', 'IDH2', 'RUNX1', 'EGFR', 'IDH1', 'ATRX', 'CIC', 
-                    'ARID1A', 'VHL', 'PBRM1', 'SETD2', 'BAP1', 'MET', 'KMT2C', 'PKHD1', 'ICE1', 
-                    'SMAD4', 'CDKN2A', 'BRAF', 'DNAH5', 'PCLO', 'LRP2', 'FREM2', 'FLG', 'TG', 'HRAS', 
-                    'KMT2D', 'BTG2', 'B2M', 'PIM1', 'IGHG1'];
-    return (
-      <div class="grid grid-cols-4 gap-4 justify-between border border-gray-200 rounded-lg px-5 pt-5 pb-5">
-        {somaticMutations.map((ssms) => 
-        <div class="flex basis-1/4 items-center me-4">
-          <input id="ssms-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-          <label for="ssms-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{ssms}</label>
-        </div>)}
+  const initialFormikValues = Object.assign({}, initialOtherVals, initialSSMSVals)
+
+  const formik = useFormik({
+    initialValues: initialFormikValues,
+    onSubmit:values => {
+      alert(JSON.stringify(values, null, 2));
+      // try {
+      //   axios.post(process.env.REACT_APP_API_URL, values)
+      //   .then((res) => {alert(res.data)});
+      // } catch(err) {
+      //   if (err && err instanceof AxiosError)
+      //     setError(err.response?.data.message);
+      //   else if (err && err instanceof Error) setError(err.message);
+        
+      //   alert(err);
+      // }
+    },
+  });
+
+  return (
+    <>
+      <Head>
+        <title>tcg.ai - Cancer Classification Using Genomic Data</title>
+        <meta
+          name="description"
+          content="tcg.ai is an AI tool to predict cancer types based on genomic data"
+        />
+        <link rel="icon" href="/tcgai_icon.png" />
+      </Head>
+      <Navbar />
+      <SectionTitle
+        pretitle="TCG.AI"
+        title="Predict Cancer Type">
+        Input patient information and any available simple somatic mutations.
+      </SectionTitle>
+      <div className="flex w-auto flex-col px-72">
+        <form onSubmit={formik.handleSubmit} className="flex flex-col bg-gray-25 shadow-md rounded px-60 pt-6 pb-8 mb-4 dark:bg-gray-700 dark:border-gray-600 dark:text-white items center justify center">
+          <h3 className="mb-4 font-semibold text-teal-500 dark:text-white text-start">Patient Race/Ethnicity</h3>
+          <select name="races" id="races" onChange={formik.handleChange} required className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500">
+            <option>White</option>
+            <option>Black</option>
+            <option>Asian</option>
+            <option>Other</option>
+          </select>
+
+          <h3 className="pt-8 mb-4 font-semibold text-teal-500 dark:text-white text-start">Patient Age</h3>
+          <input name="age" type="text" id="age" onChange={formik.handleChange} required className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500"></input>
+
+          <h3 className="pt-8 mb-4 font-semibold text-teal-500 dark:text-white text-start">Prior Malignancy</h3>
+          <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+              <input id="PMYes" type="radio" value="1" name="PriorMalignancy" onChange={formik.handleChange} required className="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label for="PMYes" className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Yes</label>
+          </div>
+          <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+              <input id="PMNo" type="radio" value="0" name="PriorMalignancy" onChange={formik.handleChange} required className="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label for="PMNo" className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">No</label>
+          </div>
+
+          <h3 className="pt-8 mb-4 font-semibold text-teal-500 dark:text-white text-start">Synchronous Malignancy</h3>
+          <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+              <input id="SMYes" type="radio" value="1" name="SynchronousMalignancy" onChange={formik.handleChange} required className="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label for="SMYes" className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Yes</label>
+          </div>
+          <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+              <input id="SMNo" type="radio" value="0" name="SynchronousMalignancy" onChange={formik.handleChange} required className="w-4 h-4 text-teal-600 bg-gray-100 border-gray-300 focus:ring-teal-500 dark:focus:ring-teal-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label for="SMNo" className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">No</label>
+          </div>
+
+          <h3 className="pt-8 mb-4 font-semibold text-teal-500 dark:text-white text-start">Simple Somatic Mutations</h3>
+          <div className="grid grid-cols-4 gap-4 content-center items-center justify-center border border-gray-200 rounded-lg px-5 pt-5 pb-5">
+            {somaticMutations.map((ssms) => 
+            <div className="flex items-center me-4">
+              <input id="ssms-checkbox" name={ssms} type="checkbox" value="" onChange={formik.handleChange} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+              <label for="ssms-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{ssms}</label>
+            </div>)}
+          </div>
+
+          <button type="submit" className="flex w-36 mt-12 mb-8 bg-teal-500 hover:bg-teal-400 text-white font-bold py-2 px-4 rounded items-center justify-center content-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
+            </svg>
+            <span className="pl-2 text-lg">Predict</span>
+          </button>
+          {error && <p>{error}</p>}
+        </form>
       </div>
-    );
-  }
+      <Footer />
+    </>
+  );
+}
 
-  export default Predict;
+export default Predict;
